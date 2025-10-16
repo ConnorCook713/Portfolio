d56ccb11b241db06d931c70359bee86db80648d1
ConnorCook713 <89484351+ConnorCook713@users.noreply.github.com>
2025-10-16 13:07:32 -0400

    Update AD_Query.ps1

--- Code/AD_Query.ps1
+++ Code/AD_Query.ps1
@@ -1,5 +1,10 @@
 ﻿Import-Module ActiveDirectory
 
+# Set the folder path here
+$Output_Path = "C:\Your\Folder\Path\lastlogondate.csv"
+
 Get-ADComputer -Filter "*" -Property Name, LastLogonDate |
 Select-Object Name, LastLogonDate |
-Sort-Object LastLogonDate -Descending
+Sort-Object LastLogonDate -Descending | 
+Export-Csv -Path $Output_Path -NoTypeInformation
+
